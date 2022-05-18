3bed32b323a428707285b06e2fc5982023ed99e8
thihuli <thiagocandidof@hotmail.com>
2022-05-18 16:04:40 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Hello</h1>
+        <GlobalStyles/>
       </header>
     </div>
   );
