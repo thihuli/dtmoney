793f25cb65653270f5f7ea761b17f111c48003b1
thihuli <thiagocandidof@hotmail.com>
2022-05-20 15:58:42 -0300

    feat: add export SVGs from variable

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import logoImg from './logo.svg';
+import close from './close.svg';
+import income from './income.svg'
+import outcome from  './outcome.svg'
+import total from './total.svg';
+
+export const Imgs = {
+  logoImg,
+  close,
+  income,
+  outcome,
+  total,
+}
