be87a0d7ce6f555cdb3d09265cd6c893b9a0b89b
thihuli <thiagocandidof@hotmail.com>
2022-05-20 15:58:04 -0300

    feat: add news global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyles = createGlobalStyle`
   :root { //definindo variaveis com cores
     --background: #f0f2f5;
     --red: #e52e4d;
+    --green: #33CC95;
     --blue: #5429cc;
 
     --blue-light: #6933ff;
@@ -32,14 +33,23 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     background: var(--background);
-    -webkit-font-smoothing: antialiased;
+    -webkit-font-smoothing: antialiased; // hackzinho para deixar a fonte com uma melhor visualização
+  }
+
+  body, input, textarea, button { // definindo font para essas tag pois não herdam do bady
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
-    cursor: pointer;
+    cursor: pointer; //define que todos os buttons terá cursosr pointer
   }
 
-  [disabled] {
+  [disabled] { // define essa estilização parat toda tag que estiver disabled
     opacity: 0.6;
     cursor: not-allowed;
   }
