4da7289ec679b6e4a9dda9d9e8594a0119d8bebd
thihuli <thiagocandidof@hotmail.com>
2022-05-23 16:05:12 -0300

    feat: add mirage.js fake db and api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer, Model } from 'miragejs';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de sistema',
+          type: 'deposit',
+          amount: 6500,
+          category: 'Desenvolvimento',
+          createdAt: new Date('2022-05-20 10:47:15'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          amount: 1200,
+          category: 'Casa',
+          createdAt: new Date('2022-05-10 14:57:15'),
+        }
+      ]
+    })
+  },
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
